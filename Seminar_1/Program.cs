b5b7ada357c4f9f4e9a2935c54c76bc45f22dc65
Viking5776 <Viking5776@yandex.ru>
2022-12-06 03:24:56 +0300

    Add Seminar_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,58 @@
+﻿// Console.Write("Введите первое число: ");
+// int first_num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int second_num = Convert.ToInt32(Console.ReadLine());
+// if (first_num > second_num){
+//     Console.WriteLine($"{first_num} больше чем {second_num}");
+// }
+// else if (first_num < second_num){
+//     Console.WriteLine($"{second_num} больше чем {first_num}");
+// }
+// else{
+//     Console.WriteLine($"{second_num} равно {first_num}");
+// }
+
+
+
+
+// Console.Write("Введите первое число: ");
+// int first_num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int second_num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int third_num = Convert.ToInt32(Console.ReadLine());
+// int max = first_num;
+// if (second_num > max){
+//     max = second_num;
+// }
+// if (third_num > max){
+//     max = third_num;
+// }
+// Console.WriteLine($"{max} - наибольшее из данных чисел.");
+
+
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num%2 == 0){
+//     Console.WriteLine($"{num} - четное число.");
+// }
+// else{
+//     Console.WriteLine($"{num} - нечетное число.");
+// }
+
+
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 2;
+// while (num < count){
+//     Console.Write($"Число меньше {count}. Введите число: ");
+//     num = Convert.ToInt32(Console.ReadLine());
+// }
+// while (count <= num){
+//     Console.Write($"{count}, ");
+//     count +=2;
+// }
